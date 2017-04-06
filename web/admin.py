341957e3a58ae0fefe23fada4f3f7f66f4388300
rahim1234 <rahim.yekta@gmail.com>
2017-04-07 02:46:00 +0430

    Update admin.py
    برای اینکه تو قسمت ادمین بهتر دیده بشه

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Expense, Income, Token, News
 # Register your models here.
-
-admin.site.register(Expense)
-admin.site.register(Income)
+class Menubar(admin.ModelAdmin):
+    list_display =["user","date","amount"]
+    
+admin.site.register(Expense,Menubar)
+admin.site.register(Income,Menubar)
 admin.site.register(Token)
 admin.site.register(News)
