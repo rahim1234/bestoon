21758871bf0911c50af2b33d1dc253c76a5a910d
rahim1234 <rahim.yekta@gmail.com>
2017-04-10 23:24:50 +0430

    افزودن فرم خود به این قسمت
    اگه فایل submit_expence.html 
    رو اضافه کنی به تمپلت به نظرم بهتر از محیط کامند میشه البته الان با کامند کار میکنه ولی مشکلش جیسونه که آخر تراکنش پیغام خوبی بر نمیگردونه
    
    پی نوشت : ببخشید تازه با گیت آشنا شدم و هنوز نمی دونم چه استاندارد هایی رو رعایت کنم فایل 
    forms.py
    رو هم باید اضافه کرد

--- web/views.py
+++ web/views.py
@@ -234,21 +234,30 @@ def submit_income(request):
 
 # submit an expanse to system (api) , input : token(POST) , output :
 # status = (ok)
+from .forms import ExpenceForm
 @csrf_exempt
 @require_POST
-def submit_expense(request):
-    """ submit an expense """
+def submit_expence (request):
+    form = ExpenceForm(request.POST or None)
+    if request.method == "POST":
+        print('requset post = ',request.POST)
+    if request.method == "GET":
+        print('requset Get = ',request.GET)
+    if form.is_valid():
+        print('cleaned_data = ', form.cleaned_data)
+        form_token = form.cleaned_data.get('token')
+        if form.cleaned_data.get('date') == None:
 
-    # TODO: revise validation for the amount
-    this_date = request.POST['date'] if 'date' in request.POST else timezone.now()
-    this_text = request.POST['text'] if 'text' in request.POST else ""
-    this_amount = request.POST['amount'] if 'amount' in request.POST else "0"
-    this_token = request.POST['token'] if 'token' in request.POST else ""
-    this_user = get_object_or_404(User, token__token=this_token)
+            date = datetime.now()
+        else:
+            date = form.cleaned_data.get('date')
+        form_text = form.cleaned_data.get('text')
+        form_amount = form.cleaned_data.get('amount')
+        form_user = get_object_or_404(User,token__token=form_token)
+        print('this form = ', form_token,' ',form_text,' ',form_user,' ',form_amount)
+        Expence.objects.create(user=form_user,amount=form_amount,text=form_text,date=date)
 
-    Expense.objects.create(user=this_user, amount=this_amount,
-                           text=this_text, date=this_date)
 
-    return JsonResponse({
-        'status': 'ok',
-    }, encoder=JSONEncoder)  # return {'status':'ok'}
+
+    context={"form":form,"title":"Submit Expence","submit":"submit"}
+    return render(request,'submit_expence.html',context)
